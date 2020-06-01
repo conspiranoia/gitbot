7a7ffb9740f0066f665df60e32aabd0780641271
Yayo Acosta <yayo.ct@gmail.com>
2020-06-01 09:18:58 +0200

    upi

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ PR By:          ${ghactor}
 [Build log here](https://github.com/${repo}/commit/${sha}/checks)`
 
         case "push":
-            return "PUSH!"
+            return platform
 // `
 // ✅ ¡Nuevas builds de desarrollo!
 // Los cambios de ${ghactor} ya están disponibles. Descárgalos [aquí](https://github.com/${repo}/actions/runs/)
